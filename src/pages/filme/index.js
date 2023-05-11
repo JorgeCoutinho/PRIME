7d9046a50f1d0574925dd74a47796c22e6298f5a
JORGE MIKAEL COUTINHO ALVES <jmca.lic19@uea.edu.br>
2023-05-11 14:31:52 -0300

    ação trailer filme

--- src/pages/filme/index.js
+++ src/pages/filme/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import './filme-info.css';
 import api from "../../services/api";
+
 
 function Filme() {
 
     const { id } = useParams();
+    const navigate = useNavigate();
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true)
 
@@ -18,17 +21,19 @@ function Filme() {
             })
                 .then((response) => {
                     setFilme(response.data);
+                    console.log(response.data)
                     setLoading(false);
                 })
                 .catch(() => {
-
+                    navigate('/', { replace:true })
+                    return
                 })
         }
 
         loadFilme();
 
         return () => {
-            console.log('fkskfnks')
+            console.log('Desmontandi')
         }
 
         if (loading) {
@@ -39,15 +44,24 @@ function Filme() {
             )
         }
 
-    }, [])
+    }, [navigate,id])
 
     return (
-        <div className="filmes-info">
+        <div className="filme-info">
             <h1>{filme.title}</h1>
             <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
             <h3>Sinopse</h3>
             <span>{filme.overview}</span>
             <strong>Avaliação: {filme.vote_average} /10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
+                        Trailer
+                    </a>
+                </button>
+            </div>
         </div>
     )
 }
