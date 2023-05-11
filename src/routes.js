e75988964daa26fc70c14eb631263f9e24f1c9b5
JORGE MIKAEL COUTINHO ALVES <jmca.lic19@uea.edu.br>
2023-05-11 15:52:01 -0300

    Excluindo Filme

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Filme from './pages/filme'
 import Header  from "./components/Header";
 import Erro from "./pages/Erro";
+import Favoritos from "./pages/Favoritos";
 
 function RoutesApp() {
     return (
@@ -12,8 +13,8 @@ function RoutesApp() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/filme/:id" element={<Filme />} />
-
                 <Route path="*" element={ <Erro/> } />
+                <Route path="/favoritos" element={<Favoritos/>} />
             </Routes>
         </BrowserRouter>
     )
